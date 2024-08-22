6d4731e3c8bdf9162f2538349ee8b3c7d9af9ddc
ktorres <ktorresno@gmail.com>
2024-08-22 09:47:06 -0600

    Error handler using AppError class to  customize our error message depending on the node environment.

--- src/interface/middleware/errorHandler.ts
+++ src/interface/middleware/errorHandler.ts
@@ -1,8 +1,55 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response, NextFunction } from 'express';
-import { HttpCode } from '../../core/constants';
+import { Request, Response } from 'express';
+import { HttpCode, NodeEnv, ErrMessagesDefaults } from '../../core/constants';
+import { envs } from '../../core/config/env';
+import { AppError } from './AppError';
 
-export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+function productionError (err: AppError, res: Response) {
+    // Operational error: send message to client about the error
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    } else {
+        // Sends a generic message to the client about the error.
+        res.status(HttpCode.INTERNAL_SERVER_ERROR).json({
+            status: ErrMessagesDefaults.ERR_STR,
+            message: ErrMessagesDefaults.INTERNAL_SERVER_ERROR
+        });
+    }
+};
+
+const developmentError = (err: AppError, res: Response) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        error: err,
+        stack: err.stack
+    });
+}
+
+function getInfoItem(infoItem: any, defaultValue: any) {
+    return infoItem || defaultValue;
+}
+
+export function errorHandler(err: any, req: Request, res: Response) {
     console.error(err.stack);
-    res.status(HttpCode.INTERNAL_SERVER_ERROR).json({ message: "Internal Server Error" });
+
+    err.statusCode = getInfoItem(err.statusCode, HttpCode.INTERNAL_SERVER_ERROR);
+    err.status = getInfoItem(err.status, ErrMessagesDefaults.ERR_STR) ;
+
+    const error = { ...err };
+    console.log("------- begin -------");
+    console.log("Error: ", error);
+    console.log("------- end -------");
+    checkErrorByEnv(error, res);
+}
+
+function checkErrorByEnv(err: AppError, res: Response) {
+    if (envs.NODE_ENV === NodeEnv.ENV_PROD) {
+        productionError(err, res);
+    } else {
+        developmentError(err, res);
+    }
 }
