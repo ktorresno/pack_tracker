e70273e8bf4ff09862287af395f2e1e1db0ccacd
ktorres <ktorresno@gmail.com>
2024-09-12 11:10:49 -0600

    Defining status for parcel

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -35,3 +35,29 @@ export enum LogLevels {
     DEBUG = 'debug',
     SILLY = 'silly'
 };
+
+export enum DeliveryStatus {
+    Shipped = "Shipped", // The delivery service has the package and customer has been given a tracking number.
+    InTransit = "In transit", // The parcel is on its way to the recipient.
+    OutForDelivery = "Out for delivery", // On the last phase. The order is expected to be delivered shortly.
+    Delivered = "Delivered", // The package is received successfully.
+    AttemptedDelivery = "Attempted delivery", // Some issues prevented the delivering process.
+    Canceled = "Canceled", // Due to reasons like stock issues or payment problems.
+    HeldAtCustoms = "Held at customs", // Await clearance for the package.
+    AwaitingPickup = "Awaiting pickup", // Customer can collect their ready at the courier pickup location.
+    Delayed = "Delayed", // External factors have postponed the delivery.
+    Lost = "Lost", // The delivery network can't locate the parcel.
+};
+
+export enum DeliveryStatusMessage {
+    Shipped = "The delivery service has the package and customer has been given a tracking number.",
+    InTransit = "The parcel is on its way to the recipient.",
+    OutForDelivery = "On the last phase. The order is expected to be delivered shortly.",
+    Delivered = "The package is received successfully.",
+    AttemptedDelivery = "Some issues prevented the delivering process.",
+    Canceled = "Due to reasons like stock issues or payment problems.",
+    HeldAtCustoms = "Await clearance for the package.",
+    AwaitingPickup = "Customer can collect their ready at the courier pickup location.",
+    Delayed = "External factors have postponed the delivery.",
+    Lost = "The delivery network can't locate the parcel.",
+};
